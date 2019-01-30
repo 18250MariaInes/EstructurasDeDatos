2e2244238665b74c8b60d4cf4f2edafa8151fdd6
18250MariaInes <35271285+18250MariaInes@users.noreply.github.com>
2019-01-30 14:22:19 -0600

    primer commit

---
+++ ProgramaCalculadora/src/programacalculadora/MyCalculator.java
@@ -0,0 +1,31 @@
+package programacalculadora;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author maria
+ */
+public class MyCalculator implements Calculator {
+    @Override
+    public int calculate (int num1, int num2, String op){
+        int dato1 = num1;
+        int dato2 = num2;
+        int resul;
+        String signo = op;
+        if (signo.equals("+")){
+            resul=dato1+dato2;
+        }else if (signo.equals("-")){
+            resul=dato1-dato2;
+        }else if (signo.equals("/")){
+            resul=dato1/dato2;
+        }else{
+            resul=dato1*dato2;
+        } 
+        return resul;
+    }
+}
