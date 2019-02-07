d61959e3cb8566297a11f39887655a0b2be3894f
Camila <gon18398@uvg.edu.gt>
2019-02-06 19:40:48 -0600

    Creacion de arreglo con 3000 numero random y almacenamiento de fichero en carpeta.

--- ProgramaDeOrdenar/src/programadeordenar/ProgramaDeOrdenar.java
+++ ProgramaDeOrdenar/src/programadeordenar/ProgramaDeOrdenar.java
@@ -5,9 +5,14 @@
  */
 package programadeordenar;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -19,12 +24,37 @@ public class ProgramaDeOrdenar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<Integer> numeros = new ArrayList<>(3000);
-        Random random = new Random();
-        for(int i=0; i < 3000; i++){
-            int numRand = random.nextInt();
-            numeros.add(numRand);
+        String ruta = "";
+        //Se intancia un objeto para generar el arreglo
+        Generador g = new Generador();
+        g.generar();
+        int[] datos = g.getNumeros();
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese la ruta del la carpeta para guardar el archivo.\nDebe ser de la siguiente forma:\nC:/Users/GrupoLF/Documents/GitHub/EstructurasDeDatos/ProgramaDeOrdenar/numeros.txt\n");
+        ruta =scan.next();
+        
+        //Se crea el archivo
+        try{
+            //String ruta = "C:/Users/GrupoLF/Documents/GitHub/EstructurasDeDatos/ProgramaDeOrdenar/numeros.txt";
+            File file = new File(ruta);
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (int i = 0; i < 3000; i++){
+                int dato = datos[i];
+                bw.write(dato + ", ");
+            }
+            bw.close();
+            System.out.println("Archivo exitosamente creado");
+        } catch (IOException e) {
+            System.out.println("Imposible crear");
         }
+       
+      
     }
     
 }
