6a3ba976841a8f4d7196879cda6041327c1cc7f3
Camila <gon18398@uvg.edu.gt>
2019-02-06 19:43:57 -0600

    Instancia de generador y almacenmiento de fichero en carpeta desde el main

---
+++ ProgramaDeOrdenar/src/programadeordenar/Generador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programadeordenar;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase que genera numeros random y los guarda en un archivo .txt
+ * @author Camila
+ */
+public class Generador {
+    int[] numeros = new int[3000];
+    
+    public void generar(){
+        Random random = new Random();
+        for(int i=0; i < 3000; i++){
+            int numRand = random.nextInt();
+            this.numeros[i] = numRand;
+        }
+    }
+    
+    public int[] getNumeros(){
+       return numeros;
+    }
+  
+}
+
+           
+ 
